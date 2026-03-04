a8d999943d184dc4b0ae448802a7657e20fd1dbe
MaciejWojs <misiek111403@gmail.com>
2026-03-04 12:56:12 +0100

    feat: add Pinia for state management and update dependencies

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -3,6 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import { routes } from 'vue-router/auto-routes'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
 const router = createRouter({
@@ -10,6 +11,9 @@ const router = createRouter({
   routes
 })
 
+const pinia = createPinia()
+
 const app = createApp(App)
 app.use(router)
+app.use(pinia)
 app.mount('#app')
