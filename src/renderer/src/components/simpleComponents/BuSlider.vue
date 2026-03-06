dd600cc7e03918c374ba055508841ec3c03b4bf4
kluchoo <dominikzucho@gmail.com>
2026-03-06 23:13:51 +0100

    fix:  formatting and fixing the error with missing component

---
+++ src/renderer/src/components/simpleComponents/BuSlider.vue
@@ -0,0 +1,34 @@
+<template>
+  <USlider
+    :model-value="props.modelValue"
+    :min="props.min"
+    :max="props.max"
+    :step="props.step"
+    :disabled="props.disabled"
+  />
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  modelValue: {
+    type: Number,
+    default: 0
+  },
+  min: {
+    type: Number,
+    default: 0
+  },
+  max: {
+    type: Number,
+    default: 100
+  },
+  step: {
+    type: Number,
+    default: 1
+  },
+  disabled: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
