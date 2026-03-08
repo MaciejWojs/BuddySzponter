640392b7075746290122846bf2f55e4660b03f65
kluchoo <dominikzucho@gmail.com>
2026-03-08 14:49:06 +0100

    feat: add alias for images in Vite and TypeScript configuration

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -12,7 +12,8 @@ export default defineConfig({
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'),
+        '@images': resolve('src/renderer/src/assets/images')
       }
     },
     plugins: [
