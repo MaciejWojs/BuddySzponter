18cb3b86efb0df71bb79c255e73ac1bf330b6b95
MaciejWojs <misiek111403@gmail.com>
2026-03-14 20:29:47 +0100

    fix: remove unused apiUtils exposure in preload script

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -19,6 +19,4 @@ if (process.contextIsolated) {
   window.electron = electronAPI
   // @ts-ignore (define in dts)
   window.api = api
-  // @ts-ignore (define in dts)
-  window.apiUtils = apiUtils
 }
