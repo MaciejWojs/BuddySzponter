0ecc09be342b7ca6c9b380888bf764b508c2aa22
kluchoo <dominikzucho@gmail.com>
2026-03-18 18:15:25 +0100

    feat: update fallback locale to Polish and initialize English translations

--- src/renderer/src/i18n.ts
+++ src/renderer/src/i18n.ts
@@ -1,14 +1,13 @@
 import { createI18n } from 'vue-i18n'
-import en from './locales/en.json'
 import pl from './locales/pl.json'
 
 const i18n = createI18n({
   legacy: false,
   locale: 'pl',
-  fallbackLocale: 'en',
+  fallbackLocale: 'pl',
   messages: {
     pl,
-    en
+    en: {}
   }
 })
 
