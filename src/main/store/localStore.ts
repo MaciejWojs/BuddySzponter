2ef5ae5959f30913d2f707c8e2f58fc60a35ff47
kluchoo <dominikzucho@gmail.com>
2026-03-18 21:31:41 +0100

    feat: refactor localStore to use shared StoreClass for ESM/CJS compatibility

--- src/main/store/localStore.ts
+++ src/main/store/localStore.ts
@@ -1,12 +1,20 @@
 import Store from 'electron-store'
-import { AppLanguage } from '../schemas/langSchemas'
+import { AppLanguage } from '../../shared/schemas/langSchemas'
+
+// Definiujemy strukturę, jakiej spodziewamy się w przypadku importu ESM w CJS
+type StoreModule = { default: typeof Store }
+
+// Obejście problemu ESM/CJS bez użycia 'any'
+const StoreClass = (
+  typeof Store === 'function' ? Store : (Store as unknown as StoreModule).default
+) as typeof Store
 
 interface LocalStoreSchema {
   language: AppLanguage
   hardwareId: string | null
 }
 
-export const localStore = new Store<LocalStoreSchema>({
+export const localStore = new StoreClass<LocalStoreSchema>({
   name: 'app-settings',
   defaults: {
     language: 'pl',
