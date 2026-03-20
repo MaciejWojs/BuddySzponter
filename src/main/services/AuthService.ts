d3f0e85e36d4ae06106bbbd8011120ca1f305030
kluchoo <dominikzucho@gmail.com>
2026-03-20 21:06:57 +0100

    feat: implement refresh token handling in AuthService

--- src/main/services/AuthService.ts
+++ src/main/services/AuthService.ts
@@ -1,7 +1,9 @@
 import { ipcMain } from 'electron'
-import { RegisterInput } from '../schemas/authSchemas'
+import { LoginInput, RegisterInput } from '../schemas/authSchemas'
 import { register } from '../handlers/auth/register'
 import { login } from '../handlers/auth/login'
+import { API_ROUTES } from '../apiRoutes'
+import { secureStore } from '../store/secureStore'
 
 export class AuthService {
   private static instance: AuthService
@@ -17,13 +19,46 @@ export class AuthService {
     return AuthService.instance
   }
 
+  public catchRefreshToken(path: string, response: Response): void {
+    if (path === API_ROUTES.AUTH.LOGIN) {
+      try {
+        const setCookieHeaders = response.headers.getSetCookie()
+
+        if (setCookieHeaders && setCookieHeaders.length > 0) {
+          const refreshTokenCookie = setCookieHeaders.find((cookie) =>
+            cookie.startsWith('refreshToken=')
+          )
+
+          if (refreshTokenCookie) {
+            const rawValue = refreshTokenCookie.split(';')[0]
+            const refreshToken = rawValue.split('=')[1]
+
+            if (refreshToken) {
+              console.log(
+                '[AuthService] Refresh token captured from login response and stored securely.',
+                refreshToken
+              )
+              secureStore.setSecure('refreshToken', refreshToken)
+            }
+          }
+        }
+      } catch (e) {
+        console.warn("Can't parse refresh token from headers:", e)
+      }
+    }
+  }
+
+  getRefreshToken(): string | undefined {
+    return secureStore.getSecure('refreshToken')
+  }
+
   // --- AUTHENTICATION METHODS ---
 
   public registerHandler(): void {
     ipcMain.handle('auth:register', async (_event, data: RegisterInput) => {
       return await register(data)
     })
-    ipcMain.handle('auth:login', async (_event, data) => {
+    ipcMain.handle('auth:login', async (_event, data: LoginInput) => {
       return await login(data)
     })
   }
