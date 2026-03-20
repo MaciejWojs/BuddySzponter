40d352604e873997e4c7d95594a2b53d4bd5e27d
kluchoo <dominikzucho@gmail.com>
2026-03-20 21:07:09 +0100

    feat: enhance securePost function with encryption handling and session validation

--- src/main/utils/apiClient.ts
+++ src/main/utils/apiClient.ts
@@ -4,35 +4,49 @@ import { encryptPayload } from '../utils/encrypt-payload'
 import { decryptPayload } from './decrypt-payload'
 import { execute } from './execute'
 import { ApiResult, ApiResultSchema, ErrorResponse } from '../schemas/apiResultSchema'
+import { authService } from '../services/AuthService'
+import { API_ROUTES } from '../apiRoutes'
+import { EncryptedPayload } from '../schemas/encryptedPayload.schema'
 
 export async function securePost(route: string, data: object): Promise<ApiResult> {
+  const isEncryptionEnabled = import.meta.env.VITE_ENCRYPT_DATA === 'true'
+
   const key = secureStore.getSecure('aesKey')
   const id = secureStore.getSecure('sessionId')
 
-  if (!key || !id) {
-    throw new Error('No session found. Please complete the handshake first.')
+  let finalData: EncryptedPayload | object = data
+  const headers: Record<string, string> = {
+    'Content-Type': 'application/json'
+  }
+
+  if (isEncryptionEnabled) {
+    if (!key || !id) {
+      throw new Error('No session found. Please complete the handshake first.')
+    }
+
+    headers['X-session-id'] = id
+
+    const aesKeyBuffer = Buffer.from(key, 'base64')
+    const encrypted = encryptPayload(data, aesKeyBuffer)
+    finalData = { payload: encrypted }
   }
 
   const baseURL = import.meta.env.VITE_API_BASE_URL
   const url = `${baseURL}${route}`
-  const aesKeyBuffer = Buffer.from(key, 'base64')
-
-  const encrypted = encryptPayload(data, aesKeyBuffer)
 
   const response = await execute(async () => {
     return await fetch(url, {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-session-id': id
-      },
-      body: JSON.stringify({ payload: encrypted })
+      headers: headers,
+      body: JSON.stringify(finalData)
     })
   })
 
-  const encryptedJson = await response.json()
+  API_ROUTES.AUTH.LOGIN === route && authService.catchRefreshToken(route, response)
 
-  const decrypted = decryptPayload(encryptedJson, key)
+  const responseJson = await response.json()
+
+  const decrypted = isEncryptionEnabled && key ? decryptPayload(responseJson, key) : responseJson
 
   const finalResult: ApiResult = response.ok
     ? { success: true, code: response.status, data: decrypted }
