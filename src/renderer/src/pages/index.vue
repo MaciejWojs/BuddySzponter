e1f24a253cc57a69315be3fc7c45e4b0a7a32131
Stygus <kuba.tokarczyk@onet.eu>
2026-03-21 18:14:13 +0100

    feat: replace BuLanguageButton with Menu component in home page

--- src/renderer/src/pages/index.vue
+++ src/renderer/src/pages/index.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="home-page">
-    <BuLanguageButton />
+    <Menu />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Menu from './Menu.vue'
+</script>
 
 <style scoped>
 .home-page {
