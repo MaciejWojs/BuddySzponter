5c5aa2626d5fe97b526155d95a3293dc20d52cff
MaciejWojs <misiek111403@gmail.com>
2026-04-04 01:17:14 +0200

    feat: add command line switch to disable background timer throttling

--- src/main/index.ts
+++ src/main/index.ts
@@ -52,7 +52,11 @@ function createWindow(): void {
 app.whenReady().then(async () => {
   // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
-
+  
+  app.commandLine.appendSwitch('disable-background-timer-throttling');
+  // app.commandLine.appendSwitch('disable-renderer-backgrounding');
+  // app.commandLine.appendSwitch('disable-backgrounding-occluded-windows');
+  
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
   // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
