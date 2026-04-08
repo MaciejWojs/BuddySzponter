84808f72c6f013afe8cf971ad802c6f85bc6eaba
kluchoo <dominikzucho@gmail.com>
2026-04-08 23:20:19 +0200

    feat: add WebSocket handlers for access, connection, handshake, and WebRTC functionalities

---
+++ src/main/handlers/socket/connection.ts
@@ -0,0 +1,48 @@
+// handlers/socket/connection.ts
+
+import { io, Socket } from 'socket.io-client'
+import { secureStore } from '../../store/secureStore'
+import { authService } from '../../services/AuthService'
+import { WsService } from '../../services/ws/WsService'
+
+export async function connect(connectionToken: string): Promise<Socket> {
+  const sessionId = secureStore.getSecure('sessionId')
+  const authToken = authService.getAccessToken()
+  const isRemote = import.meta.env.VITE_WEBRTC_REMOTE === 'true'
+  const url = isRemote ? import.meta.env.VITE_API_WS : 'http://localhost'
+
+  const activeSocket = io(url, {
+    auth: { authToken: `Bearer ${authToken}`, connectionToken, sessionId },
+    reconnection: true,
+    withCredentials: true,
+    transports: ['websocket']
+  })
+
+  return activeSocket
+}
+
+export function disconnect(socket: Socket): void {
+  if (socket) {
+    socket.emit('disconnect')
+    socket.disconnect()
+  }
+}
+
+// --- LISTENERS ---
+
+export function setupConnectionListeners(socket: Socket, service: WsService): void {
+  socket.on('connect', () => {
+    console.log('[WsListeners] Połączono z serwerem WebSocket')
+    service.handleConnected(socket.id || '')
+  })
+
+  socket.on('disconnect', (reason: string) => {
+    console.log(`[WsListeners] Gniazdko padło: ${reason}`)
+    service.handleDisconnected(reason)
+  })
+
+  socket.on('connect_error', (err: Error) => {
+    console.error(`[WsListeners] Błąd połączenia: ${err.message}`)
+    service.handleConnectError(err.message)
+  })
+}
